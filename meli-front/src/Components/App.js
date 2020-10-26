3306a569e5b5d457f83c89b8dc76093e389ce56c
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-10-25 22:22:54 -0300

    Componente App
    - Cambios de namespaces
    - Test de montaje
    - Se movió y modificó el archivo de estilos

--- meli-front/src/Components/App.js
+++ meli-front/src/Components/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom'
-import SearchBar from './SearchBar'
-import SearchResults from '../pages/SearchResults'
-import ProductDetail from '../pages/ProductDetail'
-import './App.scss';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import SearchBar from './SearchBar/SearchBar';
+import SearchResults from '../pages/SearchResults/SearchResults';
+import ProductDetail from '../pages/ProductDetail/ProductDetail';
+import '../styles/common.scss';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
-          <SearchBar />
-      <Switch>
-        <Route path="/items">   
-          <SearchResults />
-        </Route>
-        <Route path="/details/:id">
-          <ProductDetail />
-        </Route>
-      </Switch>
-    </BrowserRouter> 
+        <SearchBar />
+        <Switch>
+          <Route path="/items">   
+            <SearchResults />
+          </Route>
+          <Route path="/details/:id">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </BrowserRouter> 
     </div>
   );
 }
