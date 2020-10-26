b9378452e9eaf6a7a27ded0fb53eb9d06e6142bf
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-10-26 01:06:33 -0300

    Page Home

---
+++ meli-front/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => (
+    <main className="home">
+        <h1 className="home-title">¡Buscá un producto!</h1>
+    </main>
+);
+
+export default Home;
