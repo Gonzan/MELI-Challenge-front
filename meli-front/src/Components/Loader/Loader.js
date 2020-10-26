bf641ebb9e825e271b0dd0c6a4ed39184bfd28aa
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-10-26 01:05:50 -0300

    Componente Loader

---
+++ meli-front/src/Components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => (
+    <div className="loader-container">
+        <div class="loader"></div>
+    </div>
+);
+
+export default Loader;
